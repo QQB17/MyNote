0095582d7dc9d11578794903cb0d47c5f029ec5f
Bao <baoqq8@gmail.com>
2021-03-09 23:11:48 +0800

    Add mutex

--- basic_c++/thread.cpp
+++ basic_c++/thread.cpp
@@ -1,10 +1,15 @@
 #include <thread>
 #include <iostream>
+#include <mutex>
 
-bool finished = false; 
+char c[512]; // share variable
+bool finished = false;
+std::mutex m;
+int count = 0;
 
-void send(char* c) {
+void send() {
 	std::cout << "Start sending...\n";
+	std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	while (!finished) {
 		std::cout << "\nSend: ";
 		std::cin.getline(c,512);
@@ -16,34 +21,43 @@ void send(char* c) {
 	std::cout << "Closing send.\n";
 }
 
-void recv(char* c) {
+void recv() {
 	std::cout << "Start receive...\n";
 	while (!finished) {
 		if (strlen(c) != NULL) {
 			std::cout << "Recv: " << c << std::endl;
-			std::this_thread::sleep_for(std::chrono::microseconds(100));
 			memset(c, 0, sizeof(c));
 		}
 	}
 	std::cout << "Closing recv.\n";
 }
 
+void jobMutex() {
+	// mutex can prevent synchronization problem between two thread when the variable was shared in thread.
+	m.lock(); // lock current thread
+	count += 1;
+	std::cout << "Job " << count << " started." << std::endl;
+	std::cout << "Job " << count << " finished." << std::endl;
+	m.unlock(); // unlock current threads
+}
+
+
 int main() {
-	char c[512] = "Welcome"; // share variable
+	//std::thread t1(send);
+	//std::thread t2(recv);
 
-	std::thread t1(send, c);
-	std::thread t2(recv, c);
-	
-	std::cout << "Thread send id: " << t1.get_id() << std::endl; 
-	std::cout << "Thread recv id: " << t2.get_id() << std::endl;
+	//std::cout << "Thread send id: " << t1.get_id() << std::endl; 
+	//std::cout << "Thread recv id: " << t2.get_id() << std::endl;
 
-	std::cout << "Number of Hardware Concurrency: " << t1.hardware_concurrency() << " " << 
-		t2.hardware_concurrency() << std::endl;
+	//std::cout << "Number of Hardware Concurrency: " << t1.hardware_concurrency() << " " << t2.hardware_concurrency() << std::endl;
 
-	// synchronize threads;
-	t1.join();
-	t2.join();
+	//// synchronize threads;
+	//t1.join();
+	//t2.join();
+
+	std::thread t3(jobMutex), t4(jobMutex);
+	t3.join(), t4.join();
+
 
 	return 0;
-	
 }
