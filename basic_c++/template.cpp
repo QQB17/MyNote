8e020fcbb258eae68b719d053814052f1efb6091
Bao <baoqq8@gmail.com>
2021-03-26 20:23:31 +0800

    Update: variadic template

--- basic_c++/template.cpp
+++ basic_c++/template.cpp
@@ -1,4 +1,6 @@
+#include <sstream>
 #include <iostream>
+#include <vector>
 
 // template is to pass data type as a parameter
 // No need to write the same code with different type
@@ -15,13 +17,11 @@
 	template <tempalte <typename> typename T> class C{}; // invalid
 
 */
+
 template<typename T>
-T findMax(T x, T y) {
-	return (x > y) ? x : y;
-}
+T findMax(T x, T y) { return (x > y) ? x : y; }
 
-
-//calss template
+//class template
 template<typename T>
 class Arr {
 private:
@@ -43,24 +43,46 @@ Arr<T>::Arr(T arr[], int s) {
 
 template <typename T>
 void Arr<T>::print() {
-	for (int i = 0;i < size;i++) {
+	for (int i = 0;i < size;i++) 
 		std::cout << *(ptr + i) << " ";
-	}
+
 	std::cout << std::endl;
 }
 
-
-// call differenct type argument 
+// call different type argument 
 template <typename T, typename U>
 class moreArg {
 	T x;
 	U y;
 public:
-	moreArg() { 
-		std::cout << x  << " " << y << std::endl;
-		std::cout << "Constructor called." << std::endl; 
-	}
+	moreArg() { std::cout << "Call differenct type argument: " << x << " " << y << std::endl; }
 };
+
+//variadic template, output with sstream
+template <typename... Args>
+std::vector<std::string> convert_to_string(const Args&... args) {
+
+	// lambda recursion
+	const auto to_string_impl = [](const auto& p) {
+		std::stringstream ss;
+		ss << p;
+		return ss.str();
+	};
+
+	return { to_string_impl(args)... };
+}
+
+
+template <typename... Args>
+void get_vector(const Args&... args) {
+	const auto& v = { convert_to_string(args)... }; // something wrong here 
+	for (const auto x : v) std::cout << &x << std::endl;
+	for (const auto* x = v.begin(); x != v.begin();x++) {
+		std::cout << &x << std::endl;
+	}
+	std::cout << &v;
+	std::cout << "vector";
+}
 
 int main() {
 	std::cout << findMax(10, 5) << std::endl; // int
@@ -71,8 +93,12 @@ int main() {
 	Arr<int> a(arr, 5);
 	a.print();
 
-	moreArg<int, double> c;
-	moreArg<char, float> d;
+	const auto vec = convert_to_string("hello", 1, 23.44);
+	for (auto& o : vec) {
+		std::cout << o << std::endl;
+	}
+
+	get_vector(3.142, 50, "ok");
 
 	return 0;
 }
