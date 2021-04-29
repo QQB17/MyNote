4fd8f75e578cac420c74405bcb500c580d352b39
Bao <baoqq8@gmail.com>
2021-04-29 22:56:37 +0800

    Upload: create sql in VS

---
+++ Database/Microsoft_DB/dbo/Tables/UserName.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[UserName] (
+    [Id]      INT           IDENTITY (1, 1) NOT NULL,
+    [Name]    VARCHAR (50)  NOT NULL,
+    [Address] VARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
